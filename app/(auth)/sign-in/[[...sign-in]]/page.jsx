f369632f3874d442c50ebe30b3a0b2a7bd71d240
askinterview-ai <askinterviewai@gmail.com>
2025-03-08 23:20:30 +0530

    Update page.jsx

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -62,12 +62,11 @@ export default function Page() {
           </a>
 
           <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-            Welcome to Squid 🦑
+           Welcome to HiredAI 🦑
           </h1>
 
           <p className="mt-4 leading-relaxed text-gray-500">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-            quibusdam aperiam voluptatum.
+        AI-powered mock interview tool to enhance skills, boost confidence, and ace your next job.
           </p>
         </div>
 
